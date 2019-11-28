ae7ab22c460a71124752a50db0efa060d2685370
Kohlife <support@kohlife.com>
2019-11-28 18:40:10 +0700

    first commit

--- src/components/KohWidget.js
+++ src/components/KohWidget.js
@@ -299,7 +299,7 @@ class KohWidget extends React.PureComponent {
     })
 
     // console.log('handleArrival >>> ', Route, data[0].id)
-    // test commit #1
+    // test commit #2
 
     let Arrival = []
     let checkArrival = ''
